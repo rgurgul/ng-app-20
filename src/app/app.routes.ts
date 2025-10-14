57e07e744e09c314c31812e1b7e273104e361c10
rgurgul <gurgul.robert@gmail.com>
2025-10-14 09:46:35 +0200

    2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Items } from './pages/components/items/items';
+import { Cart } from './pages/components/cart/cart';
+import { Workers } from './pages/components/workers/workers';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'items', component: Items },
+  { path: 'workers', component: Workers },
+  { path: 'cart', component: Cart },
+];
---
+++ src/app/pages/components/workers/workers.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workers',
+  imports: [],
+  templateUrl: './workers.html',
+  styleUrl: './workers.css'
+})
+export class Workers {
+
+}
