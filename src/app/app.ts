933ddfc0f2d55f5a303b184d96462606d3e7dfbe
rgurgul <gurgul.robert@gmail.com>
2025-10-14 10:30:54 +0200

    x

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { MainComponent } from "./core/components/main/main.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MainComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
