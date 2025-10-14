1abe1a5dd11bf541a4ee7252cfc99e58e5876c40
rgurgul <gurgul.robert@gmail.com>
2025-10-14 16:22:55 +0200

    x

--- src/app/shared/components/search/search.ts
+++ src/app/shared/components/search/search.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './search.html',
   styleUrl: './search.css'
 })
 export class Search {
-
+  controls:any = input.required();
 }
